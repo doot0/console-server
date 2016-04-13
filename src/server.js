424175c04f092928eb3709d78b8805b66ccb5c2b
doot0 <doot0@doot0.co.uk>
2016-04-13 20:28:05 +0100

    added usernames

--- src/server.js
+++ src/server.js
@@ -3,13 +3,23 @@ var io = require('socket.io')(8432)
 io.on('connection', function( sock ) {
 
   sock.on('user connect', function( data ){
-    console.log('user ' + data.uuid + ' connected')
+    console.log(data.username + ', ' + data.uuid + ' connected')
+    io.emit('user connect', data);
+  })
+
+  sock.on('user namechange', function( data ){
+    console.log(data.old + " changed their name to " + data.new)
+
+    io.emit('user namechange', {
+      old: data.old,
+      new: data.new
+    })
+
   })
 
   sock.on('message', function( data ){
-    var body = data.body
-    console.log(body)
-    io.emit('message', body)
+    console.log(data)
+    io.emit('message', data)
   })
 
   sock.on('disconnect', function( data ){
